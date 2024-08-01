32fd55543c9ff9389baff30f06c4ded8dc8ec385
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-08-01 19:48:10 -0300

    Criando Layout

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -14,6 +14,6 @@ class SeriesController extends Controller
         ];
         
         // return view('listar-series', compact('series'));
-        return view('listar-series') -> with('series', $series);
+        return view('series.index') -> with('series', $series);
     }
 }
