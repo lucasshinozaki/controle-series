808147950140b7504db2928cc782f2f52cb5a28e
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-08-04 17:34:40 -0300

    Eloquente ORM

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,16 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request) {
-        $series = [
-            'Punisher', 
-            'Lost',
-            'Grey\'s Anatomy'
-        ];
+    public function index() {
+        $series = Serie::query()->orderBy('nome')->get();
         
         // return view('listar-series', compact('series'));
         return view('series.index') -> with('series', $series);
@@ -19,5 +17,13 @@ class SeriesController extends Controller
 
     public function create() {
         return view('series.create');
+    }
+
+    public function store(Request $request) {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+        return redirect('/series');
     }
 }
