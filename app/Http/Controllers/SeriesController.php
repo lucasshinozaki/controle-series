9b32763e30c71cc100d369fd7142f35cf6fbc27f
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-09-03 14:48:25 -0300

    Repository

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $repository) {
+    }
+
     public function index(Request $request) {
         $series = Series::all();
 
@@ -25,27 +26,7 @@ class SeriesController extends Controller
     }
 
     public function store(SeriesFormRequest $request) {
-        $seasons = [];
-        $serie = Series::create($request->all());
-
-        for($i = 1; $i <= $request->seasonsQty; $i++) {
-            $seasons[] = [
-                'series_id' => $serie->id,
-                'number' => $i,
-            ];
-        }
-        Season::insert($seasons);
-        $episodes = [];
-        foreach($serie->seasons as $season)  {
-            for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                $episodes[] = [
-                    'season_id'  => $season->id,
-                    'number' => $j,
-                ];
-            }
-        }
-        Episode::insert($episodes);
-        
+        $serie = $this->repository->add($request);
 
         // session(['messagem.sucesso' => 'Serie adicionada com sucesso']);
         //$request->session()->flash('messagem.sucesso', "Serie: {$serie->nome} adicionada com sucesso");
