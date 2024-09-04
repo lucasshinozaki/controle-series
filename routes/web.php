62d7e46debc10adebaa4a47b7532278e9b5ce60f
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-09-04 10:40:09 -0300

    Criando episodes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\EpisodesController;
 use App\Http\Controllers\SeasonsController;
 use App\Http\Controllers\SeriesController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +14,11 @@ Route::resource('/series', SeriesController::class)
     ->except(['show']);
 
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes', function (Request $request) {
+    dd($request->all());
+});
 
 // Route::delete('/series/destroy/{serie}', [SeriesController::class, 'destroy'])
 //     ->name('series.destroy');
